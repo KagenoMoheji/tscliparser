cdc369312c3c9a019ebef2949d3c620923db68b0
KagenoMoheji <reeg.gold.vercerms@gmail.com>
2021-04-27 23:38:03 +0900

    webpack&tsconfigでd.tsを含めたトランスパイルをしてできた成果物．

---
+++ workspace/dist/sample/main/displayHtml.d.ts
@@ -0,0 +1,26 @@
+import { CliParams, OptionAdvanced, ValueAdvanced, HelpOption } from "~~/tscliparser/types/typesTsCliParser";
+import { CliParser } from "~~/tscliparser/tscliparser";
+interface ParamsDisplayHtml extends CliParams {
+    host: {
+        data: string;
+        advanced: OptionAdvanced;
+    };
+    port: {
+        data: number;
+        advanced: OptionAdvanced;
+    };
+    html: {
+        data: string;
+        advanced: ValueAdvanced;
+    };
+    help: HelpOption;
+    test: {
+        data: false;
+        advanced: {
+            required: false;
+            short: "";
+        };
+    };
+}
+export declare const cliparser: CliParser<ParamsDisplayHtml>;
+export {};
---
+++ workspace/dist/tscliparser/errhandle/errorsTsCliParser.d.ts
@@ -0,0 +1,6 @@
+export declare class CliParserConstructorError extends Error {
+    constructor(e?: string);
+}
+export declare class CliParserArgError extends Error {
+    constructor(e?: string);
+}
